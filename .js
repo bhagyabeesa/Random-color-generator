7241fc5ec9a6f2b10731135ef28fbbdf50df6712
bhagyabeesa <113686223+bhagyabeesa@users.noreply.github.com>
2023-02-18 20:49:24 +0530

    bookmark

--- .js
+++ .js
@@ -1,49 +1,43 @@
-let color_input = document.getElementById('bgColorInput');
+// Example Valid URLs: https://learning.ccbp.in/, https://www.google.com/
+let bookmarkForm = document.getElementById("bookmarkForm");
+let siteNameInput = document.getElementById("siteNameInput");
+let siteUrlInput = document.getElementById('siteUrlInput');
+let submitBtn = document.getElementById("submitBtn");
+let siteNameErrMsg = document.getElementById("siteNameErrMsg");
+let siteUrlErrMsg = document.getElementById("siteUrlErrMsg");
+let bookmarksList = document.getElementById("bookmarksList");
 
-let font_input = document.getElementById('fontColorInput');
-
-let font_size = document.getElementById('fontSizeInput');
-
-let font_weight = document.getElementById('fontWeightInput');
-
-let padding_input = document.getElementById('paddingInput');
-
-let border_input = document.getElementById('borderRadiusInput');
-
-let my_button = document.getElementById("customButton");
-
-
-
-
-
-
-
-function applly() {
-
-    let color_value = color_input.value;
-
-    let font_value = font_input.value;
-
-    let font_size_value = font_size.value;
-
-    let font_w_value = font_weight.value;
-
-    let padding_value = padding_input.value;
-
-    let border_value = border_input.value;
-
-
-
-    my_button.style.backgroundColor = color_value;
-
-    my_button.style.color = font_value;
-
-    my_button.style.fontSize = font_size_value;
-
-    my_button.style.fontWeight = font_w_value;
-
-    my_button.style.padding = padding_value;
-
-    my_button.style.borderRadius = border_value;
-
+function addBookmark() {
+    let siteName = siteNameInput.value;
+    let siteURL = siteUrlInput.value;
+    let listItem = document.createElement("li");
+    bookmarksList.appendChild(listItem);
+    let bookmarkName = document.createElement("p");
+    let bookmarkurl = document.createElement("a");
+    bookmarkName.textContent = siteName;
+    bookmarkurl.textContent = siteURL;
+    bookmarkurl.href = siteURL;
+    bookmarkurl.setAttribute("target", "_blank");
+    bookmarksList.classList.toggle("d-none");
+    listItem.appendChild(bookmarkName);
+    listItem.appendChild(bookmarkurl);
 }
+siteUrlInput.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        siteUrlErrMsg.textContent = "Required*";
+    } else {
+        siteUrlErrMsg.textContent = "";
+    }
+});
+siteNameInput.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        siteNameErrMsg.textContent = "Required*";
+    } else {
+        siteNameErrMsg.textContent = "";
+    }
+});
+
+bookmarkForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBookmark();
+});
